6520755477102bbfd3c78a52c2b89259cf94feb1
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-09-30 20:26:17 +0200

    Db unprocessed nodes are treated as deleted ones

--- classes/menu/db.php
+++ classes/menu/db.php
@@ -13,21 +13,32 @@ class Menu_Db extends Menu_Driver
 
 	protected function _load()
 	{
-		$tree = Model_Menu::forge()->set_tree_id($this->id)->root()->get_one()->dump_tree();
+		$root = Model_Menu::forge()->set_tree_id($this->id)->root()->get_one();
+		if (is_null($root))
+		{
+			throw new MenuException('Menu #' . $this->id . ' not found');
+		}
+
+		$this->root = $root->id;
+		$tree = $root->dump_tree();
 		$tree = reset($tree);
-		$this->root = \Arr::get($tree, 'id');
 		return \Arr::get($tree, 'children', array());
 	}
 
 	protected function _update(array $menu, $root = null)
 	{
-		// No root passed, so get the tree root
+		// No root passed, so get the tree root node
 		if (is_null($root))
 		{
 			$root = Model_Menu::forge()->find($this->root);
 		}
 
-		$previous = null;
+		// Previous menu item id
+		$previous  = null;
+
+		// Processed menu items
+		// Declared static so recursion can set it's value
+		static $processed = array();
 
 		foreach ($menu as $item)
 		{
@@ -38,7 +49,7 @@ class Menu_Db extends Menu_Driver
 			// If model is not found then forge it
 			is_null($model) and $model = Model_Menu::forge();
 
-			// Skip tree fields
+			// Skip tree fields and primary keys
 			$skip_fields = \Arr::merge(Model_Menu::primary_key(), Model_Menu::tree_config(), array('children'));
 			\Arr::delete($skip_fields, array('read-only', 'title_field'));
 
@@ -66,12 +77,32 @@ class Menu_Db extends Menu_Driver
 				$model->next_sibling($model_prev)->save();
 			}
 
+			// Is this a new menu item? Get it's id
+			is_null($id) and $id = $model->id;
+
 			// Set the previous node id
 			$previous = $id;
+
+			// This menu item is processed
+			$processed[] = $id;
 
 			// If there are children then call the function again
 			empty($item['children']) or $this->_update($item['children'], $model);
 		}
+
+		// We are back from recusion: the current root is the tree root node
+		if ($root->is_root())
+		{
+			// Remove processed nodes from all nodes, so we can delete unused ones
+			$nodes = $root->descendants()->get();
+			\Arr::delete($nodes, $processed);
+
+			foreach ($nodes as $id => $model)
+			{
+				$model->delete();
+			}
+		}
+
 		return true;
 	}
 
@@ -79,7 +110,7 @@ class Menu_Db extends Menu_Driver
 	{
 		$root = Model_Menu::forge()->find($this->root);
 
-		// Skip this fields
+		// Skip tree fields and primary keys
 		$skip_fields = \Arr::merge(Model_Menu::primary_key(), Model_Menu::tree_config(), array('children'));
 		\Arr::delete($skip_fields, array('read-only', 'title_field'));
 
